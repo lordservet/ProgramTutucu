66eb68037dfe8d9b35d80c38fd7c435384cb79ad
lordservet <rdservetoksuz@gmail.com>
2021-02-09 13:33:01 +0300

    Add files via upload

---
+++ Program Tutucu/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Program_Tutucu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.adT = new System.Windows.Forms.TextBox();
+            this.adresT = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.file = new System.Windows.Forms.OpenFileDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(31, 136);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(271, 290);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(361, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Çılar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(361, 258);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Aç";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Dosya Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Dosya Adresi";
+            // 
+            // adT
+            // 
+            this.adT.Location = new System.Drawing.Point(99, 18);
+            this.adT.Name = "adT";
+            this.adT.Size = new System.Drawing.Size(229, 20);
+            this.adT.TabIndex = 6;
+            // 
+            // adresT
+            // 
+            this.adresT.Location = new System.Drawing.Point(99, 54);
+            this.adresT.Name = "adresT";
+            this.adresT.Size = new System.Drawing.Size(229, 20);
+            this.adresT.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(347, 54);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Gözat";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // file
+            // 
+            this.file.FileName = "openFileDialog1";
+            this.file.Filter = "exe dosyaları|*.exe|tüm dosyalar|*.*";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(361, 174);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Güncelle";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 442);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.adresT);
+            this.Controls.Add(this.adT);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox adT;
+        private System.Windows.Forms.TextBox adresT;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog file;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Program Tutucu/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Program_Tutucu
+{
+    using System;
+    using System.Text;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<Dosya> dosya=new List<Dosya>();
+
+        private string s;
+        private void button1_Click(object sender, System.EventArgs e)//ekle
+        {
+            dosya.Add(new Dosya(this.adT.Text, this.adres(adresT.Text)));
+            this.listele();
+            this.xml_ekle();
+        }
+
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            this.liste_sil(this.s);
+            this.listele();
+            this.xml_ekle();
+        }
+
+        private void button3_Click(object sender, System.EventArgs e)
+        {
+            Process.Start(@"" +this.bul(s)+ "");
+        }
+
+        private string bul(string ad)
+        {
+            return (from dos in this.dosya where dos.Ad == ad select dos.Adres).FirstOrDefault();
+        }
+
+        private void liste_sil(string ad)
+        {
+            dosya.RemoveAll(dos => dos.Ad == ad);
+        }
+        private void listele()
+        {
+            listBox1.Items.Clear();
+            foreach (Dosya dos in this.dosya)
+            {
+                this.listBox1.Items.Add(dos.Ad);
+            }
+        }
+       
+        private void list_ekle()
+        {
+            XmlDocument xml = new XmlDocument();
+            try
+            {
+               
+                xml.Load(@"" + Application.StartupPath + "\\xml.xml");
+                foreach (XmlNode xmlNode in xml.SelectNodes("/dosya/dosyaN"))
+                {
+                    this.dosya.Add(new Dosya(xmlNode.Attributes["Ad"].Value, xmlNode.Attributes["Adres"].Value));
+                    this.listele();
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+            
+        }
+
+        private void xml_ekle()
+        {
+            XmlDocument xml = new XmlDocument();
+            XmlNode dosyaa = xml.CreateElement("dosya");
+            xml.AppendChild(dosyaa);
+            foreach (Dosya dos in dosya)
+            {
+                
+                XmlNode dosyaNode = xml.CreateElement("dosyaN");
+
+                XmlAttribute adAttribute = xml.CreateAttribute("Ad");
+                adAttribute.Value = dos.Ad;
+                dosyaNode.Attributes.Append(adAttribute);
+
+                XmlAttribute adresAttribute = xml.CreateAttribute("Adres");
+                adresAttribute.Value = dos.Adres;
+                dosyaNode.Attributes.Append(adresAttribute);
+
+                dosyaa.AppendChild(dosyaNode);
+            }
+
+            XmlTextWriter xmlolustur = new XmlTextWriter(@"" + Application.StartupPath + "\\xml.xml", Encoding.UTF8);
+            xmlolustur.Formatting = Formatting.Indented;
+            xml.WriteContentTo(xmlolustur);
+            xmlolustur.Close();
+        }
+
+        private void button4_Click(object sender, System.EventArgs e)//Gözat
+        {
+            file.ShowDialog();
+            adT.Text = file.SafeFileName;
+            adresT.Text = file.FileName;
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            try
+            {
+                this.list_ekle();
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.s = listBox1.SelectedItem.ToString();
+           this.adT.Text=s;
+            adresT.Text = bul(s);
+        }
+
+        private void guncel(string ad)
+        {
+            foreach (Dosya dos in dosya)
+            {
+                if (dos.Ad == ad)
+                {
+                    dos.Ad = adT.Text;
+                    dos.Adres = this.adres(adresT.Text);
+                }
+            }
+            this.listele();
+        }
+        private void button5_Click(object sender, EventArgs e)//güncelle
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load(@"" + Application.StartupPath + "\\xml.xml");
+            foreach (XmlNode xmlNode in xml.SelectNodes("/dosyaN"))
+            {
+                if (xmlNode.Attributes["Ad"].Value==adT.Text)
+                this.guncel(adT.Text);
+                this.listele();
+            }
+        }
+
+        private string adres(string a)
+        {
+            string b=null;
+            for (int i = 0; i < a.Length; i++)
+            {
+
+                if (Convert.ToByte(a[i]) == 92 && Convert.ToByte(a[i + 1]) != 92)
+                {
+                    b += Convert.ToChar(92).ToString();
+                    b += Convert.ToChar(92).ToString();
+                }
+                else b += a[i];
+            }
+            return b;
+        }
+    }
+
+    public class Dosya 
+    {
+       
+        private string ad;
+        private string adres;
+
+        public Dosya(string ad, string adres)
+        {
+            
+            this.Ad = ad;
+            this.Adres = adres;
+        }
+
+       
+        public string Ad
+        {
+            get { return ad; }
+            set { ad = value; }
+        }
+        public string Adres
+        {
+            get { return adres; }
+            set { adres = value; }
+        }
+
+       
+    }
+
+}
